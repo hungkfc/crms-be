5b130d715fe8ed425e9ddc73461be32c985d50a1
Nguyen Quoc Hung <hunba9@gmail.com>
2025-10-03 17:53:06 +0700

    save

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Posts extends Model
+class Post extends Model
 {
-    //
+    protected $table = 'posts';
+    protected $fillable = ['user_id', 'title', 'content'];
+    
 }
